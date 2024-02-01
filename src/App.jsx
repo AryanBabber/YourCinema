3020ed8685794cbc4267eeb580708ba0690f486b
AryanBabber <aryanbabber12@gmail.com>
2024-02-01 23:56:31 +0530

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 // import React from 'react'
-import Navigation from "./components/Navigation"
+import Navigation from "./routes/Navigation";
+import Home from "./routes/Home";
+import SignUp from "./routes/SignUp";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div className="bg-slate-700">
-      <Navigation />
-      Hello
-    </div>
-  )
-}
+	return (
+		<div>
+			<Routes>
+				<Route
+					path="/"
+					element={<Navigation />}
+				>
+					<Route
+						index
+						element={<Home />}
+					/>
+					<Route
+						path="/sign-up"
+						element={<SignUp />}
+					/>
+				</Route>
+			</Routes>
+		</div>
+	);
+};
 
-export default App
+export default App;
